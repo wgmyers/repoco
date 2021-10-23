8260f4fe65338b38a8f4e4bea67e2207731374bf
Wayne Myers <wgmyers@gmail.com>
2021-10-23 22:05:54 +0100

    Fix  lint  complaints

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -10,26 +10,26 @@ function make_list(items) {
   const list_group = document.createElement("ul");
   list_group.classList.add("list-group");
   for (const it of items) {
-      const item = document.createElement("li");
-      item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-      item.innerHTML = it.name;
-      const badge = document.createElement("span");
-      if (it.files > 0) {
-        badge.classList.add("badge", "bg-warning", "rounded-pill");
-        badge.innerHTML = it.files + (it.files > 1 ? " changes" : " change");
-      } else {
-        badge.classList.add("badge", "bg-info", "rounded-pill");
-        badge.innerHTML = "Up to date";
-      }
-      item.appendChild(badge);
-      list_group.appendChild(item);
+    const item = document.createElement("li");
+    item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+    item.innerHTML = it.name;
+    const badge = document.createElement("span");
+    if (it.files > 0) {
+      badge.classList.add("badge", "bg-warning", "rounded-pill");
+      badge.innerHTML = it.files + (it.files > 1 ? " changes" : " change");
+    } else {
+      badge.classList.add("badge", "bg-info", "rounded-pill");
+      badge.innerHTML = "Up to date";
+    }
+    item.appendChild(badge);
+    list_group.appendChild(item);
   }
   return list_group;
 }
 
 async function make_list_group() {
   const list = document.getElementById("sitelist");
-  const response = await fetch(`/api/changes`);
+  const response = await fetch("/api/changes");
 
   if (response.ok) { // if HTTP-status is 200-299
     // get the response body (the method explained below)
