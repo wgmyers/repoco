018d58ed1df7b41707c016918eee5f1bc601abaf
Wayne Myers <wgmyers@gmail.com>
2021-10-23 10:27:29 +0100

    Split / and /dashboard

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", { title: "Dashboard "});
+});
+
 router.get("/edit", (req, res) => {
   res.render("edit", { title: "Edit" });
 });
