3f47b48edfe45ae02b556d99ff6b499cbb449e55
Wayne Myers <wgmyers@gmail.com>
2021-10-30 20:19:17 +0100

    Initial commit

---
+++ models/Users.js
@@ -0,0 +1,51 @@
+// Users.js
+// mongoose schema for the user object
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const User = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    email_is_verified: {
+      type: Boolean,
+      default: false
+    },
+    password: {
+      type: String
+    },
+    level: {
+      type: String,
+      enum: ["admin", "regular"],
+      default: "regular"
+    },
+    sites: {
+      type: Array
+    },
+    settings: {
+      type: Array
+    },
+    active: {
+      type: Boolean,
+      default: true
+    },
+    joined_date: {
+      type: Date,
+      default: Date.now
+    }
+  },
+);
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", User);
