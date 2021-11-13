96de2f31a69678f0cd7358362559e89f40af2d08
Wayne Myers <wgmyers@gmail.com>
2021-11-13 22:07:16 +0000

    Ensure API routes only accessible to authorised users

--- routes/api.js
+++ routes/api.js
@@ -2,46 +2,74 @@
 
 /* Handle API routes only available to logged in users */
 
-// FIXME: auth not yet implemented
-
 const express = require("express");
 const router = express.Router();
 
 const site = require("../lib/site");
 const files = require("../lib/files");
 const git = require("../lib/git");
+const auth = require("../lib/auth");
 
 // API routes
+
 router.get("/api/filetrees", (req, res) => {
-  res.json(site.load_filetrees());
+  if (auth.is_regular(req.user)) {
+    res.json(site.load_filetrees());
+  } else {
+    res.json(auth.not_authorised());
+  }
 });
 
 router.get("/api/files/:site/:file", (req, res) => {
-  res.json(files.load_file(req.params.site, req.params.file));
+  if (auth.site_allowed(req.user, req.params.site)) {
+    res.json(files.load_file(req.params.site, req.params.file));
+  } else {
+    res.json(auth.not_authorised());
+  }
 });
 
 router.post("/api/files/:site/:file", (req, res) => {
-  res.json(files.save_file(req.params.site, req.params.file, req.body));
-})
+  if (auth.site_allowed(req.user, req.params.site)) {
+    res.json(files.save_file(req.params.site, req.params.file, req.body));
+  } else {
+    res.json(auth.not_authorised());
+  }
+});
 
 router.get("/api/changes", async (req, res) => {
-  const changes = await git.get_changes();
-  res.json(changes);
+  if (auth.is_regular(req.user)) {
+    const changes = await git.get_changes();
+    res.json(changes);
+  } else {
+    res.json(auth.not_authorised());
+  }
 });
 
 router.get("/api/publish/:site/:target", async (req, res) => {
-  const result = await git.do_publish(req.params.site, req.params.target);
-  res.json(result);
+  if (auth.site_allowed(req.user, req.params.site)) {
+    const result = await git.do_publish(req.params.site, req.params.target);
+    res.json(result);
+  } else {
+    res.json(auth.not_authorised());
+  }
 });
 
 router.get("/api/update/:site", async (req, res) => {
-  const result = await git.do_update(req.params.site);
-  res.json(result);
+  if (auth.site_allowed(req.user, req.params.site)) {
+    const result = await git.do_update(req.params.site);
+    res.json(result);
+  } else {
+    res.json(auth.not_authorised());
+  }
 });
 
 router.get("/api/revert/:site", async (req, res) => {
-  const result = await git.do_revert(req.params.site);
-  res.json(result);
+  if (auth.site_allowed(req.user, req.params.site)) {
+    const result = await git.do_revert(req.params.site);
+    res.json(result);
+  } else {
+    req.json(auth.not_authorised());
+  }
 });
 
 module.exports = router;
