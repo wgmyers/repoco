5799331d271ba16f93e03c8dbc4ef565c21e652d
Wayne Myers <wgmyers@gmail.com>
2021-11-14 17:24:33 +0000

    Fix empty menu highlight bug

--- public/js/menu.js
+++ public/js/menu.js
@@ -7,9 +7,11 @@
 function set_menu_highlight() {
   const path = window.location.pathname.substring(1) || "home";
   const selected_item = document.getElementById(`nav-${path}`);
-  const selected_link = selected_item.children[0];
-  selected_link.classList.add("active");
-  selected_link.setAttribute("aria-current", "page");
+  if (selected_item) {
+    const selected_link = selected_item.children[0];
+    selected_link.classList.add("active");
+    selected_link.setAttribute("aria-current", "page");
+  }
 }
 
 window.onload = set_menu_highlight();
