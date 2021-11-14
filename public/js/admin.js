f738bb92ec679251ec4688f42cee94148d78bb4e
Wayne Myers <wgmyers@gmail.com>
2021-11-14 00:23:26 +0000

    Pass user details through to modal

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,18 +5,35 @@
 // click_user_event
 // Bring up the modal in which we edit our user details
 function populate_modal(event) {
-  console.log("I am in the callback");
+  // Get the modal itself
+  const modal_element = document.getElementById("modal-admin");     // This is just the DOM element
+  const modal = bootstrap.Modal.getOrCreateInstance(modal_element); // This is the actual BS modal instance
+
+  // Get data out of the row we were clicked from
+  const row = event.relatedTarget;
+  const username = row.getAttribute("data-bs-username");
+  const email = row.getAttribute("data-bs-email");
+  const level = row.getAttribute("data-bs-level");
+  const sites = row.getAttribute("data-bs-sites").split("|");
+
+  // Modal title
   const modal_title = document.getElementById("modal-title");
+  modal_title.innerHTML = `Editing '${username}'`;
+
+  // Modal body
   const modal_text = document.getElementById("modal-text");
+  modal_text.innerHTML = `User ${username} has email ${email} and is level '${level}'`;
+
+  // Ok button
   const modal_ok = document.getElementById("modal-ok");
-  modal_title.innerHTML = "I Am The New Admin Modal Title";
-  modal_text.innerHTML = "I am the text in the new admin modal.";
   modal_ok.addEventListener("click", () => {
     // Do stuff here
+    console.log("I am in the ok button click handler");
     modal.hide();
-    modal.dispose();
   });
 }
 
+// Event handler to bring up modal on table row click
+// Table rows contain data-bs-* elements as required to set this up
 const modal = document.getElementById("modal-admin");
 modal.addEventListener("show.bs.modal", populate_modal);
