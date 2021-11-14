e06175317360843fa8030034e378588da0c30553
Wayne Myers <wgmyers@gmail.com>
2021-11-14 13:42:33 +0000

    Refactor so admin routes go in admin not auth

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,22 @@
 /* admin.js */
 
-/* Handle HTML routes only available to logged in admins */
+/* Handle routes only available to logged in admins:
+ * - admin page
+ * - adduser
+ * - updateuser
+ * - deluser
+ */
 
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const User = require("../models/Users");
 
 const admin = require("../lib/admin");
 const auth = require("../lib/auth");
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get("/admin", async (req, res, next) => {
   if (!req.user) {
@@ -14,6 +24,74 @@ router.get("/admin", async (req, res, next) => {
   } else if (auth.is_admin(req.user)) {
     const users = await admin.get_users();
     res.render("admin", { title: "Admin", user: req.user, users: users, messages: req.flash() });
+  } else {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.post("/adduser", urlencodedParser, (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/");
+  } else if (auth.is_admin(req.user)) {
+    User.register(new User({
+      username: req.body.username,
+      email: req.body.email,
+      level: "regular",
+      sites: []
+    }), req.body.password, (err, user) => {
+      if (err) {
+        console.log(`Error: ${err.message} with user:`);
+        console.dir(user);
+        req.flash("error", err.message);
+      } else {
+        req.flash("info", `User '${req.body.username}' created`);
+      }
+      res.redirect("/admin");
+    });
+  } else {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.post("/updateuser/:user", urlencodedParser, (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/");
+  } else if (auth.is_admin(req.user)) {
+    // Update user here
+    console.log("In update user with:");
+    console.dir(req.body);
+    res.redirect("/admin");
+  } else {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.get("/deluser/:user", (req, res, next) => {
+  if(!req.user) {
+    res.redirect("/");
+  } else if (auth.is_admin(req.user)) {
+    // Try to delete the user
+    // Report back with flash message either way
+    // NB: Specify a regular user here to make it so we cannot delete the admin user
+    User.findOneAndDelete({ username: req.params.user, level: 'regular' }, (err, result) => {
+      if (err) {
+        console.error(err);
+        req.flash("error", `Could not delete user ${req.params.user}`);
+      } else {
+        if (result) {
+          req.flash("info", `User '${req.params.user} deleted.'`);
+        } else {
+          req.flash("error", `Did not delete user ${req.params.user}`);
+        }
+      }
+      res.redirect("/admin");
+    });
   } else {
     const err = new Error("Forbidden");
     err.status = 403;
--- routes/auth.js
+++ routes/auth.js
@@ -1,78 +1,22 @@
 /* auth.js */
 
-/* All login / registration / authentication routes go here */
+/* Login/logout goes here */
 
 const express = require("express");
 const passport = require("passport");
 const bodyParser = require("body-parser");
-const User = require("../models/Users");
 const router = express.Router();
-const auth = require("../lib/auth");
 
 // create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-/* Login, add/delete user, and logout */
+/* Login, add/update/delete user, and logout */
 
 router.post("/login", urlencodedParser, passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/",
   failureFlash: true
 }));
-
-router.post("/adduser", urlencodedParser, (req, res, next) => {
-  if (!req.user) {
-    res.redirect("/");
-  } else if (auth.is_admin(req.user)) {
-    User.register(new User({
-      username: req.body.username,
-      email: req.body.email,
-      level: "regular",
-      sites: []
-    }), req.body.password, (err, user) => {
-      if (err) {
-        console.log(`Error: ${err.message} with user:`);
-        console.dir(user);
-        req.flash("error", err.message);
-      } else {
-        req.flash("info", `User '${req.body.username}' created`);
-      }
-      res.redirect("/admin");
-    });
-  } else {
-    const err = new Error("Forbidden");
-    err.status = 403;
-    next(err);
-  }
-
-});
-
-router.get("/deluser/:user", (req, res, next) => {
-  if(!req.user) {
-    res.redirect("/");
-  } else if (auth.is_admin(req.user)) {
-    // Try to delete the user
-    // Report back with flash message either way
-    // NB: Specify a regular user here to make it so we cannot delete the admin user
-    User.findOneAndDelete({ username: req.params.user, level: 'regular' }, (err, result) => {
-      if (err) {
-        console.error(err);
-        req.flash("error", `Could not delete user ${req.params.user}`);
-      } else {
-        if (result) {
-          req.flash("info", `User '${req.params.user} deleted.'`);
-        } else {
-          req.flash("error", `Did not delete user ${req.params.user}`);
-        }
-      }
-      res.redirect("/admin");
-    });
-  } else {
-    const err = new Error("Forbidden");
-    err.status = 403;
-    next(err);
-  }
-});
 
 router.get("/logout", (req, res) => {
   req.logout();
