595bca113289865232f39d9c4e8c135916a83946
Wayne Myers <wgmyers@gmail.com>
2022-05-22 14:47:05 +0100

    Add callback function to logout per Passport 0.6.0 change

--- routes/auth.js
+++ routes/auth.js
@@ -19,8 +19,10 @@ router.post("/login", urlencodedParser, passport.authenticate("local", {
 }));
 
 router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
